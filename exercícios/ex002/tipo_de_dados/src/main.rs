4da5d07780bd158c6836f0b3dca2eb701100117f
Hector2523 <heito@Hector.(none)>
2023-12-10 12:34:45 -0300

    Tipo de dados

--- exercícios/ex002/tipo_de_dados/src/main.rs
+++ exercícios/ex002/tipo_de_dados/src/main.rs
@@ -13,3 +13,21 @@
 //     println!("{}", _y);
 // }
 
+fn main() {
+    // adição
+    let soma = 5 + 10;
+
+    // subtração
+    let diferenca = 95.5 - 4.3;
+
+    // multiplicação
+    let produto = 4 * 30;
+
+    // divisão
+    let quociente = 56.7 / 32.2;
+
+    // resto
+    let resto = 43 % 5;
+
+    println!(" O valor das variáveis são: \n {soma} \n {diferenca} \n {produto} \n {quociente} \n {resto}")
+}
